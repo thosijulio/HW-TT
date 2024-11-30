5422ed66d6f3d73da26d774e8b5e87f08815f56a
thosijulio <thosijulio@gmail.com>
2024-11-30 15:47:00 -0300

    FEAT - navegacao mobile

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const formSection = document.getElementById("form-section");
-const formWarningText = document.getElementById("form-warning-text");
-const form = document.getElementById("lead-form");
 const body = document.querySelector("body");
+const formSection = document.getElementById("form-section");
+const productSection = document.getElementById("product-section");
 
+// Código referente a primeira seção (Formulário)
+
+const formWarningText = document.getElementById("form-warning-text");
+const mainHeader = document.querySelector("body > header");
+const form = document.getElementById("lead-form");
 const welcomeDiv = document.createElement("div");
 welcomeDiv.id = "welcome-div";
 
@@ -11,11 +15,13 @@ if (form) {
     ev.preventDefault();
 
     // Ocultando formulário
-    if (formSection && formWarningText) {
+    if (formSection && formWarningText && productSection && mainHeader) {
       formWarningText.style.display = "none";
+      mainHeader.style.display = "none";
       formSection.classList.add("hide");
 
       formSection.addEventListener("animationend", () => {
+        productSection.style.display = "block";
         formSection.style.display = "none";
       });
     }
@@ -26,3 +32,35 @@ if (form) {
     body.insertAdjacentElement("beforeend", welcomeDiv);
   });
 }
+
+// Código referente a segunda seção (Produto)
+
+const productHeader = document.getElementById("product-header");
+
+// Animação no Header ao sroll da página
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 30) {
+    productHeader.style.backgroundColor = "#fafdea";
+    productHeader.style.color = "#203b42";
+  } else {
+    productHeader.style.backgroundColor = "transparent";
+    productHeader.style.color = "#fafdea";
+  }
+});
+
+// Menu lateral
+const hamburger = document.getElementById("product-hamburguer-mobile");
+const sidebar = document.getElementById("product-header-nav-mobile");
+
+hamburger.addEventListener("click", (event) => {
+  sidebar.style.left = "0";
+  event.stopPropagation();
+});
+
+document.addEventListener("click", (event) => {
+  if (!sidebar.contains(event.target)) {
+    sidebar.style.left = "-100%";
+  }
+});
